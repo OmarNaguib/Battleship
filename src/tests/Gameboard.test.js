945e7e1185cc942d13a406d08c67e459a1e66c5c
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-30 23:40:02 +0300

    Test ship placement

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -4,3 +4,26 @@ test("Makes a board", () => {
   const aBoard = Gameboard();
   expect(aBoard.board).toStrictEqual(Array(100).fill(0));
 });
+
+test("Places a ship", () => {
+  const aBoard = Gameboard();
+  aBoard.placeShip(5, "h", 0);
+  expect(aBoard.board).toStrictEqual(
+    Array(5).fill(1).concat(Array(95).fill(0))
+  );
+});
+
+test("Dosen't place invalid ship: Out of bounds", () => {
+  const aBoard = Gameboard();
+  aBoard.placeShip(5, "h", 6);
+  expect(aBoard.board).toStrictEqual(Array(100).fill(0));
+});
+
+test("Dosen't place invalid ship: overlap", () => {
+  const aBoard = Gameboard();
+  aBoard.placeShip(5, "h", 0);
+  aBoard.placeShip(5, "h", 4);
+  expect(aBoard.board).toStrictEqual(
+    Array(5).fill(1).concat(Array(95).fill(0))
+  );
+});
