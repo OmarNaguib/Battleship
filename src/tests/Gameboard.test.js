176b65e61d444519de8c4375488855ab21e2865a
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-31 06:58:34 +0300

    test vertical placement

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -37,3 +37,46 @@ test("Dosen't place invalid ship: overlap", () => {
     Array(5).fill(1).concat(Array(95).fill(0))
   );
 });
+
+test("Places a ship: vertical", () => {
+  const aBoard = Gameboard();
+  aBoard.placeShip(0, "v", 5, 1);
+  expect(aBoard.getBoard()).toStrictEqual([
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+});
+
+test("Dosen't place invalid ship: Out of bounds, vertical", () => {
+  const aBoard = Gameboard();
+  aBoard.placeShip(91, "v", 6, 1);
+  console.log(aBoard.getBoard());
+  expect(aBoard.getBoard()).toStrictEqual(Array(100).fill(0));
+});
+
+test("Dosen't place invalid ship: overlap", () => {
+  const aBoard = Gameboard();
+  aBoard.placeShip(0, "v", 5, 1);
+  aBoard.placeShip(30, "v", 5, 1);
+  expect(aBoard.getBoard()).toStrictEqual([
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+});
+
+/* [
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+] */
